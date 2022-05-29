f1a09ddeddf3dfdc593e16b0b63fbfc541497ffc
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-05-29 19:27:13 +0700

    feat: dump route for testing

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.status(200).send('<h2>Hello yoi mamen</h2>').end()
 })
 
+app.get('/boy', (req, res) => {
+  res.status(200).send('<h2>Hello yoi boyy</h2>').end()
+})
+
+app.get('/kratos', (req, res) => {
+  res.status(200).send('<h2>Son of kratos</h2>').end()
+})
+
 // Start the server
 const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
