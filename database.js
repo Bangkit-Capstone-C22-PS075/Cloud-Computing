a29c9f9cf4040e87ade1dc4dc39ca92010741b16
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-06-03 05:52:50 +0700

    feat: add database config

--- database.js
+++ database.js
@@ -2,7 +2,6 @@ const mysql = require("mysql")
 
 const pool = mysql.createPool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   socketPath: process.env.INSTANCE_UNIX_SOCKET
 })
